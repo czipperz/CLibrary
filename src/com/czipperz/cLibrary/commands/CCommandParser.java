36d48a2d24dfabf09693e03d03b97122b50bcbec
Czipperz <czipperz@gmail.com>
2015-02-05 23:41:05 -0800

    Added method so that outputs can be put into a terminal instead of thrown.

--- src/com/czipperz/cLibrary/commands/CCommandParser.java
+++ src/com/czipperz/cLibrary/commands/CCommandParser.java
@@ -2,9 +2,14 @@ package com.czipperz.cLibrary.commands;
 
 import com.czipperz.cLibrary.exceptions.CCommandException;
 
+import java.security.InvalidParameterException;
+import java.util.ArrayList;
+import java.util.function.Consumer;
+
 /**
  * Created by czipperz on 1/31/15.
  */
 public interface CCommandParser {
-    public CCommandInput parse(CCommandExecutor[] possibleCommands, String input) throws CCommandException;
+    public CCommandInput parse(ArrayList<CCommandExecutor> possibleCommands, String input) throws CCommandException, InvalidParameterException;
+    public CCommandInput parse(ArrayList<CCommandExecutor> possibleCommands, String input, Consumer<String> bash);
 }
