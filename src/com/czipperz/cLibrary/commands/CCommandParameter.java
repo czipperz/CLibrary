8aea6a4893e701a976f3cf0d13d2b55c8fa602d3
Czipperz <czipperz@gmail.com>
2015-02-05 17:00:58 -0800

    Made the CCPD and CCPS classes into one.

--- src/com/czipperz/cLibrary/commands/CCommandParameter.java
+++ src/com/czipperz/cLibrary/commands/CCommandParameter.java
@@ -1,13 +1,29 @@
 package com.czipperz.cLibrary.commands;
 
+import java.util.ArrayList;
+
 /**
+ * This class takes a <i>{@link com.czipperz.cLibrary.commands.CCommandParameterType}</i> object
+ * and possible extra data provided after the switch and provides a consolidation point.
  * Created by czipperz on 1/31/15.
  */
-public abstract class CCommandParameter {
+public class CCommandParameter {
     private CCommandParameterType type;
+    private ArrayList<String> v = null;
 
     public CCommandParameter(CCommandParameterType type) {
         this.type = type;
+    }
+
+    public CCommandParameter(CCommandParameterType type, ArrayList<String> values) {
+        this(type);
+        this.v = values;
+    }
+
+    public CCommandParameter(CCommandParameterType type, String value) {
+        this(type);
+        this.v = new ArrayList<>();
+        this.v.add(value);
     }
 
     public CCommandParameterType getType() {
@@ -24,5 +40,9 @@ public abstract class CCommandParameter {
 
     public String getAliasDoubleSwitch() {
         return type.getAliasDoubleSwitch();
+    }
+
+    public ArrayList<String> getAdditionalValues() throws NullPointerException {
+        return v;
     }
 }
