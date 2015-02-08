11606db212dad2dec3b62c16ae219ac09cc3abea
Czipperz <czipperz@gmail.com>
2015-02-08 15:07:29 -0800

    Removed some un-needed methods and added a differentiator for if you need a second argument

--- src/com/czipperz/cLibrary/commands/CCommandParameterTypeBlank.java
+++ src/com/czipperz/cLibrary/commands/CCommandParameterTypeBlank.java
@@ -3,13 +3,19 @@ package com.czipperz.cLibrary.commands;
 /**
  * Created by Chris on 2/3/2015.
  */
-public class CCommandParameterTypeBlank implements CCommandParameterType {
+public class CCommandParameterTypeBlank extends CCommandParameterType {
+    private boolean needsSecondArg;
+
+    public CCommandParameterTypeBlank(boolean needsSecondArg) {
+        this.needsSecondArg = needsSecondArg;
+    }
+
     public String man() {
         return null;
     }
 
     public boolean needsSecondArg() {
-        return false;
+        return needsSecondArg;
     }
 
     public char getAliasSingleSwitch() {
@@ -18,13 +24,5 @@ public class CCommandParameterTypeBlank implements CCommandParameterType {
 
     public String getAliasDoubleSwitch() {
         return "-";
-    }
-
-    public boolean hasAliasSingleSwitch() {
-        return false;
-    }
-
-    public boolean hasAliasDoubleSwitch() {
-        return false;
     }
 }
