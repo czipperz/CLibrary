5da0b46d7e183d91ba34f599ad4ab15b8d86c6ec
Czipperz <czipperz@gmail.com>
2015-02-08 14:35:35 -0800

    Made canExecute into a default method, and added a javadoc for whyNoExecute(CCommandInput)

--- src/com/czipperz/cLibrary/commands/CCommandExecutor.java
+++ src/com/czipperz/cLibrary/commands/CCommandExecutor.java
@@ -13,7 +13,17 @@ public interface CCommandExecutor {
     public CCommandExecutor execute(CCommandInput input, Consumer<String> bash);
     public CCommandExecutor execute(CCommandInput input) throws CParameterException;
 
-    public boolean canExecute(CCommandInput input);
+    public default boolean canExecute(CCommandInput input) {
+        return input.getCommandExecutor().getCallName().equals(this.getCallName());
+    }
+
+    /**
+     * Returns the reason that {@link #canExecute(com.czipperz.cLibrary.commands.CCommandInput)} cannot execute.
+     * Returns null if it can't execute the input.
+     * @param input the {@link com.czipperz.cLibrary.commands.CCommandInput} that it doesn't work on.
+     * @return the reason (in Exception form) of why the {@link com.czipperz.cLibrary.commands.CCommandInput} can't execute.
+     * @see com.czipperz.cLibrary.commands.CCommandExecutor
+     */
     public CCommandException whyNoExecute(CCommandInput input);
 
     public CCommandExecutor man(Consumer<String> bash);
