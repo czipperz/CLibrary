6d688ff3baa27f914decc9b02fb49fd84210fbfb
Czipperz <czipperz@gmail.com>
2015-02-08 14:36:16 -0800

    Started work on a Man command.

---
+++ src/com/czipperz/cLibrary/commands/CCommandExecutorMan.java
@@ -0,0 +1,39 @@
+package com.czipperz.cLibrary.commands;
+
+import com.czipperz.cLibrary.exceptions.CCommandException;
+import com.czipperz.cLibrary.exceptions.CExceptionParser;
+import com.czipperz.cLibrary.exceptions.CParameterException;
+
+import java.util.ArrayList;
+import java.util.function.Consumer;
+
+/**
+ * Created by Chris on 2/8/2015.
+ */
+public class CCommandExecutorMan implements CCommandExecutor {
+    public CCommandExecutor execute(CCommandInput input, Consumer<String> bash) {
+        return man(bash);
+    }
+
+    public CCommandExecutor execute(CCommandInput input) throws CParameterException, UnsupportedOperationException {
+        throw new UnsupportedOperationException("Must call this method with a bash terminal input (no other way to man something than to print it.)");
+    }
+
+    public CCommandException whyNoExecute(CCommandInput input) {
+        return null;
+    }
+
+    public CCommandExecutor man(Consumer<String> bash) {
+        //TODO: this
+
+        return this;
+    }
+
+    public String getCallName() {
+        return "man";
+    }
+
+    public ArrayList<CCommandParameterType> getPotentialParameters() {
+        return new ArrayList<>();
+    }
+}
