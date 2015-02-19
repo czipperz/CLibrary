d05fb9097ae2d8a865adcaaea984bf7d32c3c15a
Czipperz <czipperz@gmail.com>
2015-02-19 00:16:19 -0800

    Made subclasses of CBash for System.out and System.err

---
+++ src/com/czipperz/cLibrary/CBashSErr.java
@@ -0,0 +1,10 @@
+package com.czipperz.cLibrary;
+
+/**
+ * Created by Chris on 2/19/2015.
+ */
+public class CBashSErr extends CBash {
+    public CBashSErr() {
+        super(System.err);
+    }
+}
---
+++ src/com/czipperz/cLibrary/CBashSOut.java
@@ -0,0 +1,12 @@
+package com.czipperz.cLibrary;
+
+import java.io.OutputStream;
+
+/**
+ * Created by Chris on 2/19/2015.
+ */
+public class CBashSOut extends CBash {
+    public CBashSOut() {
+        super(System.out);
+    }
+}
