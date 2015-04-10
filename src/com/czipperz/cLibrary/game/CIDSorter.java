64cbc34f8e61e98f0df5f9a63dc53856f497c5ef
Czipperz <czipperz@gmail.com>
2015-04-09 21:55:37 -0500

    Bye Java.

--- src/com/czipperz/cLibrary/game/CIDSorter.java
+++
@@ -1,9 +0,0 @@
-package com.czipperz.cLibrary.game;
-
-import java.util.Comparator;
-
-public class CIDSorter implements Comparator<CIDAble> {
-	public int compare(CIDAble id1, CIDAble id2) {
-		return id1.getInstanceID().compareTo(id2.getInstanceID());
-	}
-}
--- src/com/czipperz/cLibrary/game/CUpdateAble.java
+++
@@ -1,7 +0,0 @@
-package com.czipperz.cLibrary.game;
-
-public interface CUpdateAble {
-	public default CUpdateAble updateBefore() {return this;}
-	public CUpdateAble update();
-	public default CUpdateAble updateAfter() {return this;}
-}
