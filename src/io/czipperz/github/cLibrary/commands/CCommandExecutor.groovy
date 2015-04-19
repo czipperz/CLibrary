92853e6ba671a578b4cd1b01a1b127d9866a2fa3
Czipperz <czipperz@gmail.com>
2015-04-19 07:21:32 -0700

    Output type changed so that it can be used as an interceptor. Uses arrays not ArrayLists.

--- src/io/czipperz/github/cLibrary/commands/CCommandExecutor.groovy
+++ src/io/czipperz/github/cLibrary/commands/CCommandExecutor.groovy
@@ -5,19 +5,19 @@ import io.czipperz.github.cLibrary.exceptions.CCommandException
 import io.czipperz.github.cLibrary.exceptions.CParameterException
 
 /**
- * Created by czipperz on 4/9/15.
- */
+* @author czipperz on 4/9/15.
+*/
 abstract class CCommandExecutor {
-	abstract CCommandExecutor execute(CCommandInput input, CBash bash)
-	abstract CCommandExecutor execute(CCommandInput input) throws CParameterException
+	abstract CCommandInput execute(CCommandInput input, CBash bash)
+	abstract CCommandInput execute(CCommandInput input) throws CParameterException
 
 	boolean canExecute(CCommandInput input) {
-		input.commandExecutor.callName == this.callName
+		input.commandExecutor.callName.equals this.callName
 	}
 
 	abstract CCommandException whyNoExecute(CCommandInput input)
-	abstract CCommandExecutor man(CBash bash)
+	abstract void man(CBash bash)
 
 	abstract String[] getCallName()
-	abstract ArrayList<CCommandParameterType> getPotentialParameters()
+	abstract CCommandParameterType[] getPotentialParameters()
 }
