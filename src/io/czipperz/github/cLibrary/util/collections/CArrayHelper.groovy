8f5a69f1d1f2a6281c698e3fba190188fdec73e4
Czipperz <czipperz@gmail.com>
2015-04-19 07:19:51 -0700

    Removed a boolean and general better things. New template for class creation. Uses @author

--- src/io/czipperz/github/cLibrary/util/collections/CArrayHelper.groovy
+++ src/io/czipperz/github/cLibrary/util/collections/CArrayHelper.groovy
@@ -1,6 +1,9 @@
 package io.czipperz.github.cLibrary.util.collections;
 
-public final class CArrayHelper {
+/**
+ * @author czipperz
+ */
+final class CArrayHelper {
 	private CArrayHelper() {}
 
 	/**
@@ -10,18 +13,13 @@ public final class CArrayHelper {
 	 * @param equals - true means use .equals() to test, false means use ==
 	 * @return the condensed array (will == t, not literally, if no objects to delete.)
 	 */
-	public static <T extends Object> T[] condense(T[] t, T delete, boolean equals) {
+	static <T extends Object> T[] condense(T[] t, T delete) {
 		Objects.requireNonNull(t);
 		Objects.requireNonNull(delete);
 		List<T> result = toArrayList(t);
-		for(int i = 0; i < t.length; i++) {
-			if(equals)
-				if(!(t[i].equals(delete)))
-					result.remove(i);
-				else
-				if(!(t[i] == delete))
-					result.remove(i);
-		}
+		for(int i = 0; i < t.length; i++)
+			if(!(t[i] == delete))
+				result.remove(i);
 		return toArray(result);
 	}
 
@@ -30,7 +28,7 @@ public final class CArrayHelper {
 	 * @param t - the array to condense.
 	 * @return the condensed array (will == t, literally, if no objects to delete.)
 	 */
-	public static <T extends Object> T[] condense(T[] t) {
+	static <T extends Object> T[] condense(T[] t) {
 		Objects.requireNonNull(t);
 		List<T> result = toArrayList(t);
 		for(int i = 0; i < t.length; i++)
@@ -39,148 +37,148 @@ public final class CArrayHelper {
 		return toArray(result);
 	}
 
-	public static <T extends Object> List<T> addAll(List<T> a, List<T> t) {
+	static <T extends Object> List<T> addAll(List<T> a, List<T> t) {
 		for(T i : t)
 			a.add(i);
 		return a;
 	}
 
-	public static <T extends Object> List<T> addAll(List<T> a, T[] t) {
+	static <T extends Object> List<T> addAll(List<T> a, T[] t) {
 		for(T i : t)
 			a.add(i);
 		return a;
 	}
 
-	public static <T extends Object> ArrayList<T> toArrayList(T[] t) {
+	static <T extends Object> ArrayList<T> toArrayList(T[] t) {
 		ArrayList<T> r = new ArrayList<T>();
 		for(int i = 0; i < t.length; i++)
 			r.add(t[i]);
 		return r;
 	}
 
-	public static <T extends Object> T[] toArray(List<T> t) {
+	static <T extends Object> T[] toArray(List<T> t) {
 		return (T[]) t.toArray();
 	}
 
-	public static <E extends Object> E[] snip(List<E> a, int x, int width) {
+	static <E extends Object> E[] snip(List<E> a, int x, int width) {
 		E[] result = (E[]) new Object[width];
 		for(int i = 0; i < result.length; i++)
 			result[i] = a.get(x);
 		return result;
 	}
 
-	public static <E extends Object> E[] snip(E[] a, int x, int width) {
+	static <E extends Object> E[] snip(E[] a, int x, int width) {
 		E[] result = (E[]) new Object[width];
 		for(int i = x; i < x + width; i++)
 			result[i] = a[x];
 		return result;
 	}
 
-	public static <E extends Object> E[][] snip(E[][] a, int x, int width) {
+	static <E extends Object> E[][] snip(E[][] a, int x, int width) {
 		E[][] result = (E[][]) new Object[width][a[0].length];
 		for(int i = x; i < x + width; i++)
 			result[i] = a[x];
 		return result;
 	}
 
-	public static Object[] snipTo(List<? extends Object> a, int x, int endX) {
+	static Object[] snipTo(List<? extends Object> a, int x, int endX) {
 		Object[] result = new Object[endX - x];
 		for(int i = 0; i < result.length; i++)
 			result[i] = a.indexOf(i + x);
 		return result;
 	}
 
-	public static boolean[] snip(boolean[] a, int x, int width) {
+	static boolean[] snip(boolean[] a, int x, int width) {
 		boolean[] result = new boolean[width];
 		for(int i = x; i < x + width; i++)
 			result[i] = a[x];
 		return result;
 	}
 
-	public static boolean[][] snip(boolean[][] a, int x, int width) {
+	static boolean[][] snip(boolean[][] a, int x, int width) {
 		boolean[][] result = new boolean[width][a[0].length];
 		for(int i = x; i < x + width; i++)
 			result[i] = a[x];
 		return result;
 	}
 
-	public static char[] snip(char[] a, int x, int width) {
+	static char[] snip(char[] a, int x, int width) {
 		char[] result = new char[width];
 		for(int i = x; i < x + width; i++)
 			result[i] = a[x];
 		return result;
 	}
 
-	public static char[][] snip(char[][] a, int x, int width) {
+	static char[][] snip(char[][] a, int x, int width) {
 		char[][] result = new char[width][a[0].length];
 		for(int i = x; i < x + width; i++)
 			result[i] = a[x];
 		return result;
 	}
 
-	public static double[] snip(double[] a, int x, int width) {
+	static double[] snip(double[] a, int x, int width) {
 		double[] result = new double[width];
 		for(int i = x; i < x + width; i++)
 			result[i] = a[x];
 		return result;
 	}
 
-	public static double[][] snip(double[][] a, int x, int width) {
+	static double[][] snip(double[][] a, int x, int width) {
 		double[][] result = new double[width][a[0].length];
 		for(int i = x; i < x + width; i++)
 			result[i] = a[x];
 		return result;
 	}
 
-	public static float[] snip(float[] a, int x, int width) {
+	static float[] snip(float[] a, int x, int width) {
 		float[] result = new float[width];
 		for(int i = x; i < x + width; i++)
 			result[i] = a[x];
 		return result;
 	}
 
-	public static float[][] snip(float[][] a, int x, int width) {
+	static float[][] snip(float[][] a, int x, int width) {
 		float[][] result = new float[width][a[0].length];
 		for(int i = x; i < x + width; i++)
 			result[i] = a[x];
 		return result;
 	}
 
-	public static int[] snip(int[] a, int x, int width) {
+	static int[] snip(int[] a, int x, int width) {
 		int[] result = new int[width];
 		for(int i = x; i < x + width; i++)
 			result[i] = a[x];
 		return result;
 	}
 
-	public static int[][] snip(int[][] a, int x, int width) {
+	static int[][] snip(int[][] a, int x, int width) {
 		int[][] result = new int[width][a[0].length];
 		for(int i = x; i < x + width; i++)
 			result[i] = a[x];
 		return result;
 	}
 
-	public static long[] snip(long[] a, int x, int width) {
+	static long[] snip(long[] a, int x, int width) {
 		long[] result = new long[width];
 		for(int i = x; i < x + width; i++)
 			result[i] = a[x];
 		return result;
 	}
 
-	public static long[][] snip(long[][] a, int x, int width) {
+	static long[][] snip(long[][] a, int x, int width) {
 		long[][] result = new long[width][a[0].length];
 		for(int i = x; i < x + width; i++)
 			result[i] = a[x];
 		return result;
 	}
 
-	public static <T extends Object> ArrayList<T> createArrayList(T object) {
+	static <T extends Object> ArrayList<T> createArrayList(T object) {
 		ArrayList<T> a = new ArrayList<T>();
 		a.add(object);
 		return a;
 	}
 
-	public static <T extends Object> T[] createArray(T object) {
+	static <T extends Object> T[] createArray(T object) {
 		T[] t = (T[]) new Object[1];
 		t[0] = object;
 		return t;
@@ -191,7 +189,7 @@ public final class CArrayHelper {
 	 * @param items - The lists to compare
 	 * @return Returns true if an object is measured as equivalent between the lists.
 	 */
-	public static <T extends Object> boolean isOverlap(List<T>[] items) {
+	static <T extends Object> boolean isOverlap(List<T>[] items) {
 		for (int i = 0; i < items.length - 1; i++)
 			for (int j = 0; j < items[i].size(); j++)
 				for (int k = i+1; k < items.length; k++)
@@ -207,7 +205,7 @@ public final class CArrayHelper {
 	 * @param items - The arrays to compare
 	 * @return Returns true if an object is measured as equivalent between the lists.
 	 */
-	public static <T extends Object> boolean isOverlap(T[][] items) {
+	static <T extends Object> boolean isOverlap(T[][] items) {
 		for (int i = 0; i < items.length - 1; i++)
 			for (int j = 0; j < items[i].length; j++)
 				for (int k = i+1; k < items.length; k++)
