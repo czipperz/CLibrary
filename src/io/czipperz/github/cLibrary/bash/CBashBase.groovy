a100ac6666833270de261fb4839f91608fa45116
Chris Gregory <czipperz@gmail.com>
2015-06-16 01:31:23 -0500

    Unneeded public

--- src/io/czipperz/github/cLibrary/bash/CBashBase.groovy
+++ src/io/czipperz/github/cLibrary/bash/CBashBase.groovy
@@ -10,7 +10,7 @@ abstract class CBashBase implements CBash {
 	private final Closure flush;
 	private final Consumer<Boolean> boldConsumer;
 
-	public CBashBase(Consumer<String> stringConsumer, Closure flush, Consumer<Boolean> boldConsumer) {
+	CBashBase(Consumer<String> stringConsumer, Closure flush, Consumer<Boolean> boldConsumer) {
 		this.stringConsumer = stringConsumer
 		this.flush = flush
 		this.boldConsumer = boldConsumer
